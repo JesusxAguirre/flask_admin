a9da74f65e7e2d44b3c0d71d258b1713069b1e48
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-04-01 09:06:30 -0400

    creando archivo de configuracio

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 import os
 
 from flask import Flask
-
+from flask_security import Security, login_required, \
+     SQLAlchemySessionUserDatastore
+from database import db_session, init_db
+from models import User, Role
 
 def create_app(test_config=None):
     # create and configure the app
@@ -10,19 +13,24 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
     )
 
+    # Setup Flask-Security
+    user_datastore = SQLAlchemySessionUserDatastore(db_session,
+                                                User, Role)
+    security = Security(app, user_datastore)
+
+    # Create a user to test with
+    @app.before_first_request
+    def create_user():
+        init_db()
+        user_datastore.create_user(email='matt@nobien.net', password='password')
+        db_session.commit()
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
+    
     # a simple page that says hello
     @app.route('/hello')
     def hello():
---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+
+
+class Config:
+
+    SERVER_NAME = "localhost:5000"
+    DEBUG = "True"
+
+    #VARIABLES DE BASE DE DATOS
+    SECRET_KEY = "e23895acc525f92e886e9fe425046e0743855fbc038a70067540742c9fd34179"
+
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:admin123@localhost:5432/flask-app-post'
+
+    SQLALCHEMY_TRACK_MODIFICATION= False
+
+    #SOBREESCRIBIENDO RUTAS DE FLASK
+
+    TEMPLATE_FOLDER= "views/templates"
+    STATIC_FOLDER = "views/static"
