cd46d668876b4615c23450c5a98cd12039dd4330
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-04-04 07:58:47 -0400

    creando generador de error

---
+++ app/routes/errors.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, Response
+from ..models.exceptions import UserAlreadyExist
+
+
+errors_scope = Blueprint("errors",__name__)
+
+
+
+def __generate_error_response(error: Exception) -> Response:
+    message = {
+        "ErrorType": type(error).__name__,
+        "Message": str(error)
+    }
+
+    return message
+
+@errors_scope.errorhandler(UserAlreadyExist)
+def handler_user_already_exist(error: UserAlreadyExist)-> Response:
+    response = __generate_error_response(error)
+    response.status_code = 404
+    return response
