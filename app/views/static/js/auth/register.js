9a565ca8aee38328479c1e8c45b5f36247f6752f
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-04-07 11:32:37 -0400

    agregando sweetalert.js

--- app/views/static/js/auth/register.js
+++ app/views/static/js/auth/register.js
@@ -12,52 +12,93 @@ const campos = {
 }
 
 const expresiones = { //objeto con varias expresiones regulares
-	
-	caracteres: /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'°]{3,12}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/, // 6 a 16 digitos.
-	correo: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+
+    caracteres: /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'°]{3,12}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/, // 6 a 16 digitos.
+    correo: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
 
 }
 
 
-const validar_formulario  = (e) => {
+const validar_formulario = (e) => {
 
-    switch(e.target.name){
+    switch (e.target.name) {
         case "name":
-			validar_campo(expresiones.caracteres, e.target, 'name');
-			break;
+            validar_campo(expresiones.caracteres, e.target, 'name')
+            break;
+
+        case "apellido":
+            validar_campo(expresiones.caracteres, e.target, 'apellido')
+            break;
+
+        case "email":
+            validar_campo(expresiones.email, e.target, 'email')
+            break;
+        case "password":
+            validar_campo(expresion.password, e.target, 'password')
+
     }
 
 }
 
+const validar_campo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+        document.querySelector(`#grupo__${campo} i`).classList.remove('bi', 'bi-exclamation-triangle-fill', 'text-danger', 'input-icon');
+        document.querySelector(`#grupo__${campo} p`).classList.remove('d-block');
+        document.querySelector(`#grupo__${campo} i`).classList.add('bi', 'bi-check-circle-fill', 'text-check', 'input-icon2');
+        document.querySelector(`#grupo__${campo} p`).classList.add('d-none');
+        campos[campo] = true;
+        //comprobando si la cedula existe en la bd
 
 
-$('#formulario').submit(function(event) {
+    } else {
+        document.querySelector(`#grupo__${campo} i`).classList.remove('bi', 'bi-check-circle-fill', 'text-check', 'input-icon2');
+        document.querySelector(`#grupo__${campo} p`).classList.remove('d-none');
+        document.querySelector(`#grupo__${campo} i`).classList.add('bi', 'bi-exclamation-triangle-fill', 'text-danger', 'input-icon');
+        document.querySelector(`#grupo__${campo} p`).classList.add('d-block');
+        campos[campo] = false;
+    }
+}
+
+
+
+$('#formulario').submit(function (event) {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
-  
-    $.ajax({
-      type: 'POST',
-      url: '/register',
-      data:  $(this).serialize(),// Obtiene los datos del formulario
-      success: function(response) {
-        console.log("entro en el success")
-        // Código a ejecutar si la solicitud se realizó correctamente
-      },
-      error: function(xhr, status, error) {
-        // Código a ejecutar si se produjo un error al realizar la solicitud
-       
+    if (!(campos.name && campos.apellido && campos.email && campos.password && campos.retype_password )) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Lo siento ',
+            text: 'Registra el formulario correctamente '
+        })
+    } else {
+        $.ajax({
+            type: 'POST',
+            url: '/register',
+            data: $(this).serialize(),// Obtiene los datos del formulario
+            success: function (response) {
+                console.log("entro en el success")
+                // Código a ejecutar si la solicitud se realizó correctamente
+            },
+            error: function (xhr, status, error) {
+                // Código a ejecutar si se produjo un error al realizar la solicitud
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Lo siento ',
+                    text: 'Registra el formulario correctamente '
+                })
 
 
-        console.log(xhr.responseJSON)
-      }
-    });
-  });
+                console.log(xhr.responseJSON)
+            }
+        });
+    }
+});
 
 
 //recorriendo foreach de inputs por ada inputs se le agrega un evento y se llama la funcion validar formulario
 inputs.forEach((input) => {
-	input.addEventListener('keyup', validar_formulario);
-	input.addEventListener('blur', validar_formulario);
+    input.addEventListener('keyup', validar_formulario);
+    input.addEventListener('blur', validar_formulario);
 
 });
 
