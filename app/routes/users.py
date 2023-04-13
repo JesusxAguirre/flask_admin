1db0e1b22e27249a0ade708e7e09fad0b7779439
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-04-13 12:36:30 -0400

    agregando estilos a botones de importacion

--- app/routes/users.py
+++ app/routes/users.py
@@ -11,12 +11,10 @@ users_scope = Blueprint("users", __name__)
 @users_scope.route("/", methods=["GET"])
 @login_required
 def users_get():
-    users = user_controller.get_all()
-   
-    users = [{'id': user.id, 'name': user.name, 'email': user.email} for user in users]
+
     
 
-    return render_template("users/users.html",users=users)
+    return render_template("users/users.html")
 
 @users_scope.route("/usuarios", methods=["GET"])
 @login_required
