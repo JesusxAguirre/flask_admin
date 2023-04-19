fcbb9ef575b080fed2f8696d60b6af9cc522badf
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-04-19 09:32:02 -0400

    agregando campos de fecha de registro, direccio y telefono a la tabla usuarios

--- app/models/User.py
+++ app/models/User.py
@@ -4,6 +4,7 @@ from sqlalchemy import select
 from flask_sqlalchemy import SQLAlchemy
 from ..models.exceptions import UserAlreadyExist,UserNotExist
 
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -16,7 +17,10 @@ class User(db.Model,UserMixin):
     name = db.Column(db.String(12))
     apellido = db.Column(db.String(12))
     email = db.Column(db.String(256), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    telefono = db.Column(db.String(20))
+    direccion = db.Column(db.String(20))
+    fecha_registro = db.Column(db.Date(), default=datetime.utcnow)
     is_admin = db.Column(db.Boolean, default=False)
     rol = db.Column(db.String(30), default = "Invitado")
 
