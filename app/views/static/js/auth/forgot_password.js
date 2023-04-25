59d2a9e3389d956446256882b0100bf77f063d15
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-04-25 14:55:45 -0400

    creando toast de swetalert con timer para tiempo de 5 minutos para recuperar password, ademaas agrregando elementos html dinamicamente al formulario

--- app/views/static/js/auth/forgot_password.js
+++ app/views/static/js/auth/forgot_password.js
@@ -1,9 +1,10 @@
 //declarando constante que guarda en un array todos los inputs dentro del id formulario
+const formulario = document.getElementById("formulario")
 const inputs = document.querySelectorAll("#formulario input")
-
+const boton_submit = document.getElementById("boton_submit")
 
 const campos = {
-   
+
     email: false,
 
 }
@@ -11,8 +12,8 @@ const campos = {
 
 const expresiones = { //objeto con varias expresiones regulares
 
-    
-  
+
+
     email: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
 
 }
@@ -20,7 +21,7 @@ const expresiones = { //objeto con varias expresiones regulares
 const validar_formulario = (e) => {
 
     switch (e.target.name) {
-      
+
         case "email":
             validar_campo(expresiones.email, e.target, 'email');
             break;
@@ -60,36 +61,80 @@ $('#formulario').submit(function (event) {
             position: 'center'
         })
     } else {
-        let url = document.getElementById('url_login').value
+        let url = document.getElementById('url_forgot').value
         $.ajax({
             type: 'POST',
             url: url,
             data: $(this).serialize(),// Obtiene los datos del formulario
             success: function (response) {
-                document.getElementById("formulario").reset()
-                for (let key in campos) {
-                    campos[key] = false;
-                }
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Has iniciado seccion correctamente'
-                }).then((result) => {
-                    /* Read more about isConfirmed, isDenied below */
-                    if (result.isConfirmed) {
-                     window.location.replace(document.getElementById('url_dashboard').value);
-                    } 
-                  })
+                // Crear un elemento div
+                var div = document.createElement('div');
+                div.id = 'grupo_token_correo'
+                div.innerHTML = `
+                    <div class="input-group mb-3">
+                        <input id="token_correo" name="token_correo" type="text" class="form-control" placeholder="Codigo">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-key"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="text-danger d-none">Escriba un email valido</p>`;
 
-    
-                
-                setTimeout(function() {
-                    window.location.replace(document.getElementById('url_dashboard').value);
-                  },4000);
+                // Obtener el nodo padre del botón_submit
+                var nodo = formulario.lastElementChild;
+
+                // Insertar el elemento div antes del nodo padre del botón_submit
+                formulario.insertBefore(div, nodo);
+
+                // Deshabilitar el elemento con el id "email"
+                document.getElementById("email").setAttribute('readonly', true);
+
+                formulario.id = "formulario2"
+
+                boton_submit.form = "formulario2"
+                boton_submit.textContent = "Enviar codigo"
+                const inputs = document.querySelectorAll("#formulario2 input")
+                Swal.fire({
+                    toast: true,
+                    title: 'Se ha enviado un codigo a tu correo por favor colocalo en el campo del formulario para resetear tu password',
+                    position: 'top-end',
+                    html: 'El tiempo se acabara en <b></b> .',
+                    showConfirmButton: false,
+                    timerProgressBar: true,
+                    timer: 300000, // Tiempo en milisegundos (5 minutos = 300000 milisegundos)
+                    didOpen: () => {
+                        Swal.showLoading();
+                        const b = Swal.getHtmlContainer().querySelector('b');
+                        let contador = 300;
+                        const intervalo = setInterval(() => {
+                            contador--;
+                            // Formatear el tiempo restante en minutos y segundos
+                            const minutos = Math.floor(contador / 60);
+                            const segundos = contador % 60;
+                            b.textContent = `${minutos}:${segundos.toString().padStart(2, '0')}`;
+                            if (contador === 0) {
+                                clearInterval(intervalo);
+                                Swal.close();
+                            }
+                        }, 1000);
+                    },
+                    willClose: () => {
+                        console.log("Alerta cerrada")
+                    }
+                });
+
+
+
+
+
+
+
             },
             error: function (xhr, status, error) {
                 // Código a ejecutar si se produjo un error al realizar la solicitud
 
-               
+
                 Swal.fire({
                     icon: 'error',
                     title: xhr.responseJSON.ErrorType,
