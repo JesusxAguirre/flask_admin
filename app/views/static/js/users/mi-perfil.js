77f2718fad2dac2a61d6cef2682ece8b64c2143e
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-05-01 14:35:07 -0400

    colocando solicitud ajax. falta arreglar la logica de envio de formulario

--- app/views/static/js/users/mi-perfil.js
+++ app/views/static/js/users/mi-perfil.js
@@ -6,12 +6,12 @@ const inputs = document.querySelectorAll("#formulario input")
 
 const campos = {
     nombre: false,
-    apellido: false,
-    email : false,
-    telefono :false,
-    fechaNacimiento: false,
-    password: false,
-    direccion: false,
+    apellido: true,
+    email : true,
+    telefono :true,
+    fechaNacimiento: true,
+    password: true,
+    direccion: true,
 }
 
 
@@ -36,9 +36,22 @@ const validar_formulario = (e) => {
         case "email":
             validar_campo(expresiones.email, e.target, 'email');
             break;
+        
+        case "telefono":
+            validar_campo(expresiones.caracteres, e.target, 'telefono');
+            break;
 
+        case "fechaNacimiento":
+            validar_campo(expresiones.caracteres, e.target, 'fechaNacimiento');
+            break;
+
+    
         case "password":
             validar_campo(expresiones.password, e.target, 'password');
+            break;
+
+        case "direccion":
+            validar_campo(expresiones.caracteres, e.target, 'direccion');
             break;
 
     }
@@ -76,3 +89,60 @@ inputs.forEach((input) => {
 });
 
 
+
+
+
+
+
+
+
+
+$('#formulario').submit(function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+    if (!(campos.name && campos.apellido && campos.email && campos.password && campos.direccion && campos.fechaNacimiento && campos.telefono)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Lo siento ',
+            text: 'Registra el formulario correctamente ',
+            position: 'center'
+        })
+    } else {
+        let url=document.getElementById("url_perfil").value
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: $(this).serialize(),// Obtiene los datos del formulario
+            success: function (response) {
+                document.getElementById("formulario").reset()
+                for (let key in campos) {
+                    campos[key] = false;
+                }
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Te has registrado correctamente en el sistema'
+                })
+            },
+            error: function (xhr, status, error) {
+                // Código a ejecutar si se produjo un error al realizar la solicitud
+
+                if (xhr.responseJSON.ErrorType == "UserAlreadyExist") {
+                    document.querySelector(`#grupo__email p`).classList.remove('d-none');
+
+                    document.querySelector(`#grupo__email p`).classList.add('d-block');
+                    document.querySelector(`#grupo__email input`).classList.add('is-invalid')
+                    campos.email = false;
+
+                    document.getElementById('mensaje_email').textContent = xhr.responseJSON.Message
+                }
+                Swal.fire({
+                    icon: 'error',
+                    title: xhr.responseJSON.ErrorType,
+                    text: xhr.responseJSON.Message
+                })
+
+
+
+            }
+        });
+    }
+});
