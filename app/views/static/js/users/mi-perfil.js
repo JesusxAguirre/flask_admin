48f66b6ebf9fb28388adad1192a12c7d52775e2c
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-05-01 14:18:54 -0400

    arreglando links de js

--- app/views/static/js/users/mi-perfil.js
+++ app/views/static/js/users/mi-perfil.js
@@ -4,3 +4,75 @@ const formulario = document.querySelector("formulario")
 const inputs = document.querySelectorAll("#formulario input")
 // Obtener el elemento de entrada oculto
 
+const campos = {
+    nombre: false,
+    apellido: false,
+    email : false,
+    telefono :false,
+    fechaNacimiento: false,
+    password: false,
+    direccion: false,
+}
+
+
+const expresiones = { //objeto con varias expresiones regulares
+
+    caracteres: /^[A-ZÑa-zñáéíóúÁÉÍÓÚ'°]{3,12}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,12}$/, // 6 a 12 digitos.
+    email: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+}
+
+const validar_formulario = (e) => {
+
+    switch (e.target.name) {
+        case "name":
+            validar_campo(expresiones.caracteres, e.target, 'name');
+            break;
+
+        case "apellido":
+            validar_campo(expresiones.caracteres, e.target, 'apellido');
+            break;
+
+        case "email":
+            validar_campo(expresiones.email, e.target, 'email');
+            break;
+
+        case "password":
+            validar_campo(expresiones.password, e.target, 'password');
+            break;
+
+    }
+
+}
+
+
+const validar_campo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+
+        document.querySelector(`#grupo__${campo} p`).classList.remove('d-block');
+        document.querySelector(`#grupo__${campo} input`).classList.remove('is-invalid')
+
+        document.querySelector(`#grupo__${campo} p`).classList.add('d-none');
+        campos[campo] = true;
+
+    } else {
+
+
+        document.querySelector(`#grupo__${campo} p`).classList.remove('d-none');
+
+        document.querySelector(`#grupo__${campo} p`).classList.add('d-block');
+        document.querySelector(`#grupo__${campo} input`).classList.add('is-invalid')
+        campos[campo] = false;
+    }
+}
+
+
+
+//recorriendo foreach de inputs por ada inputs se le agrega un evento y se llama la funcion validar formulario
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validar_formulario);
+    input.addEventListener('blur', validar_formulario);
+
+});
+
+
